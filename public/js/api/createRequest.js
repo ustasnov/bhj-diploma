1f3e5073a4056f625d20cae6b164e48d2d9481f6
ustasnov <ustasnov@gmail.com>
2023-05-17 17:36:10 +0700

    Modify createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,45 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  if (!options.url) {
+    console.error('Не задан адрес запроса!');
+    return;
+  }
 
-};
+  if (!options.method) {
+    console.error('Не задан метод запроса!');
+    return;
+  }
+
+  if (!options.callback) {
+    console.error('Не задана функция обратного вызова для запроса!');
+    return;
+  }
+
+  const xhr = new XMLHttpRequest;
+  let formData = null;
+
+  if (options.data) {
+    if (options.method === 'GET') {
+      options.url += '?';
+      let i = 0;
+      for (const [key, value] of Object.entries(options.data)) {
+        options.url += i === 0 ? '' : '&' + `${key}=${value}`;
+      }
+    } else {
+      formData = new FormData();
+      for (const [key, value] of Object.entries(options.data)) {
+        formData.append(`${key},${value}`);
+      }
+    }
+  }
+
+  xhr.open(options.method, options.url);
+  xhr.setRequestHeader('Content-Type', 'json');
+  if (formData) {
+    xhr.send(formData);
+  } else {
+    xhr.send();
+  }
+
+}
